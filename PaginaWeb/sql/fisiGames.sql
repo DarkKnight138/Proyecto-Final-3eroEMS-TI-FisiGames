afb82f0495da3d0d613258a695f1730522984774
723nachito <nachito.okk@gmail.com>
2025-08-11 08:44:42 -0300

    Update fisiGames.sql

--- PaginaWeb/sql/fisiGames.sql
+++ PaginaWeb/sql/fisiGames.sql
@@ -1,3 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 11-08-2025 a las 13:33:41
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `fisigames`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cuentas`
+--
+
+CREATE TABLE `cuentas` (
+  `id_cuenta` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `contraseña` varchar(255) NOT NULL,
+  `permiso` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `cuentas`
+--
+
+INSERT INTO `cuentas` (`id_cuenta`, `nombre`, `email`, `contraseña`, `permiso`) VALUES
+(1, 'matias', 'matiaspro@gmail.com', '$2y$10$Vlh9CqQ/EpfZPbF4biOghOAhOjRXLfNkFDt26.ubEoC6jcZ/H2u6.', 1),
+(2, 'IgnaGay', 'igna@gmail.com', '$2y$10$GP/1PvQwhKUZsapsaU3Cme8Wysy5xPtceEmDFOIq7hBFomxsZhife', 1),
+(3, 'nacho', 'nacho@gmail.com', '$2y$10$Xx0F9HA4Yng.l2qMsQp9XOS492Ev11XRwGkIpOQBbGkpHFtU/Q/Ki', 1),
+(4, 'nacho', 'nachoo@gmail.com', '$2y$10$/D3eSScPVurDliYtNTcfTOn572Mt1WVwGOnIrDAk1lQ8u0oJceTca', 1),
+(5, 'francisco', 'fran@gmail.com', '$2y$10$yqqyXDHkankVd9iuk4s1Tu8t2tHM/PM.0ANndldZUMNKbzzuojbiG', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `desbloquean`
+--
+
+CREATE TABLE `desbloquean` (
+  `id_cuenta` int(11) NOT NULL,
+  `id_juego` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `grupos`
+--
+
+CREATE TABLE `grupos` (
+  `id_grupo` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `creado_por` int(11) DEFAULT NULL,
+  `contraseña` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `juegos`
+--
+
+CREATE TABLE `juegos` (
+  `id_juego` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mensajes`
+--
+
+CREATE TABLE `mensajes` (
+  `id_mensaje` int(11) NOT NULL,
+  `usr_emisor` int(11) NOT NULL,
+  `id_grupo` int(11) NOT NULL,
+  `contenido` text NOT NULL,
+  `fecha_hora` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `partidas_jugadas`
+--
+
+CREATE TABLE `partidas_jugadas` (
+  `id_partida` int(11) NOT NULL,
+  `id_cuenta` int(11) DEFAULT NULL,
+  `id_juego` int(11) DEFAULT NULL,
+  `fecha` datetime DEFAULT current_timestamp(),
+  `puntaje` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
 --
 -- Estructura de tabla para la tabla `pertenece_a`
 --
@@ -69,7 +182,7 @@ ALTER TABLE `pertenece_a`
 -- AUTO_INCREMENT de la tabla `cuentas`
 --
 ALTER TABLE `cuentas`
-  MODIFY `id_cuenta` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_cuenta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `grupos`
@@ -133,3 +246,7 @@ ALTER TABLE `pertenece_a`
   ADD CONSTRAINT `pertenece_a_ibfk_1` FOREIGN KEY (`id_cuenta`) REFERENCES `cuentas` (`id_cuenta`),
   ADD CONSTRAINT `pertenece_a_ibfk_2` FOREIGN KEY (`id_grupo`) REFERENCES `grupos` (`id_grupo`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
