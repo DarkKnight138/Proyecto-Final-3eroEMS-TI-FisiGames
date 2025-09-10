79424497c02e568e825980e4867d306c5d4d3995
DarkKnight138 <franciscorodriguezalayon2018@gmail.com>
2025-09-10 08:13:55 -0300

    Update menucentral.sh

--- Servidor/Script-Gestion-Usuarios-y-Grupos/menucentral.sh
+++ Servidor/Script-Gestion-Usuarios-y-Grupos/menucentral.sh
@@ -7,13 +7,17 @@ echo "************************************************************************" 
 echo "*                        Seleccione una opción                         *"      
 echo "* 1)Ir a la administracion de Usuarios                                 *"                   
 echo "* 2)Ir a la administracion de Grupos                                   *"
-echo "* 3)Salir                                                              *"
+echo "* 3)Ir a la administracion del Firewall                                *"
+echo "* 4)Ir a la administracion de Docker                                   *"
+echo "* 5)Salir                                                              *"
 echo "************************************************************************"
 read usu                                                                            #Lee la variable usuario y le asigna el valor ingresado por el usuario 
 case $usu in                                                                        #Realiza una accion dependiendo el valor de de la variable usuario  
 1) sh scriptusuarios.sh ;;                                                          #Ejecuta el script scriptusuarios.sh  
 2) sh scriptgrupos.sh ;;                                                            #Ejecuta el script scriptgrupos.sh  
-3) echo "Saliendo...";;                                                             #Sale del menú
-*) echo "La opcion $usu es invalida, ingrese otra";;                                              #Muestra un mensaje de error si selecciona una opcion invalida
+3) sh firewall.sh;;                                                                 #Ejecuta el script firewall.sh
+4) sh docker.sh;;                                                                   #Ejecuta el script docker.sh
+5) echo "Saliendo...";;                                                             #Sale del menú
+*) echo "La opcion $usu es invalida, ingrese otra";;                                #Muestra un mensaje de error si selecciona una opcion invalida
 esac                                                                                #Final del case
 done                                                                                #Final del do
