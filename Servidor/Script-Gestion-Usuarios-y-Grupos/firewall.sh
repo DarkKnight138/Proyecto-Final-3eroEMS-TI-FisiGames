e508ee61f0b7e8c7a7cf0c2626db49e57d57b3b3
DarkKnight138 <franciscorodriguezalayon2018@gmail.com>
2025-09-11 12:16:32 +0000

    script firewall

--- Servidor/Script-Gestion-Usuarios-y-Grupos/firewall.sh
+++ Servidor/Script-Gestion-Usuarios-y-Grupos/firewall.sh
@@ -12,6 +12,9 @@ do
     echo "* 4) Listar interfaces de red                                  *"
     echo "* 5) Listar zonas de red                                       *"
     echo "* 6) Agregar puerto/servicio                                   *"
+    echo "* 7) Bloquear puerto/servicio                                  *"
+    echo "* 8) Agregar ip+puerto                                         *"
+    echo "* 9) Bloquear ip+puerto                                        *"
     echo "* 0) Volver                                                    *"
     echo "****************************************************************"
     echo "Opcion:"
@@ -28,24 +31,65 @@ do
       do
       echo "1) Puerto"
       echo "2) Servicio"
+      echo "0) Volver"
       read eleccion
       case $eleccion in
-        1) comando="firewall-cmd --permanent --add-port="
+         1)        
         echo "Ingrese puerto:"
-        read puerto
-        comando+="$puerto"
-        sudo eval $comando;;
-        2) comando="firewall-cmd --permanent --add-service="
+        read puerto 
+        sudo firewall-cmd --permanent --add-port=$puerto/tcp
+        sudo firewall-cmd --reload
+        ;;
+        2)
         echo "Ingrese servicio:"
-        read servicio
-        comando+="servicio"
-        sudo eval $comando;;
+        read servicio 
+        sudo firewall-cmd --permanent --add-service=$servicio
+        sudo firewall-cmd --reload
+        ;; 
         0)echo "Volviendo ....";;
         *)echo "Eleccion $eleccion es invalida";;
         esac
         done
-        clear
-        
+        ;;
+      7) eleccion=1 
+      while((eleccion != 0))
+      do
+      echo "1) Puerto"
+      echo "2) Servicio"
+      echo "0) Volver"
+      read eleccion
+      case $eleccion in
+        1)        
+        echo "Ingrese puerto:"
+        read puerto 
+        sudo firewall-cmd --permanent --remove-port=$puerto/tcp
+        sudo firewall-cmd --reload
+        ;;
+        2)
+        echo "Ingrese servicio:"
+        read servicio 
+        sudo firewall-cmd --permanent --remove-service=$servicio
+        sudo firewall-cmd --reload
+        ;;
+        0)echo "Volviendo ....";;
+        *)echo "Eleccion $eleccion es invalida";;
+        esac
+        done
+        ;;
+      8) echo "Ingrese ip a agregar:"
+         read ip
+         echo "Ingrese puerto a agregar"
+         read puerto
+         sudo firewall-cmd --permanent --add-rich-rule="rule family='ipv4' source address='${ip}' port port='${puerto}' protocol='tcp' accept"
+         sudo firewall-cmd --reload
+         ;;
+      9) echo "Ingrese ip a agregar:"
+         read ip
+         echo "Ingrese puerto a agregar"
+         read puerto
+         sudo firewall-cmd --permanent --add-rich-rule="rule family='ipv4' source address='${ip}' port port='${puerto}' protocol='tcp' log prefix='conexion rechazada' limit value='1/m' reject"
+         sudo firewall-cmd --reload
+         ;;
       0) echo "Volviendo....";;
       *) echo "La opcion $opcion es invalida";;
     esac
