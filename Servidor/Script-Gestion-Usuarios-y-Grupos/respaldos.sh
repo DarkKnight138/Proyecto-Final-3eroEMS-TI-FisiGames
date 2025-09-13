bab405580afdc1525a58c2461f4ddd28c0a7b576
DarkKnight138 <franciscorodriguezalayon2018@gmail.com>
2025-09-13 13:55:04 -0300

    Update respaldos.sh

--- Servidor/Script-Gestion-Usuarios-y-Grupos/respaldos.sh
+++ Servidor/Script-Gestion-Usuarios-y-Grupos/respaldos.sh
@@ -1,1 +1,20 @@
-
+#!/bin/bash
+clear
+opcion=1
+while ((opcion != 0))
+do
+    echo "****************************************************************"
+    echo "*                    Seleccione una opción                     *"
+    echo "* 1) Realizar respaldo manual                                  *"
+    echo "* 2) Editar respaldos automaticos                              *"
+    echo "* 0) Volver                                                    *"
+    echo "****************************************************************"
+    echo "Opcion:"
+    read opcion
+    clear
+    case $opcion in
+      
+      0) sh menucentral.sh;;
+      *) echo "La opcion $opcion es invalida";;
+    esac
+done
