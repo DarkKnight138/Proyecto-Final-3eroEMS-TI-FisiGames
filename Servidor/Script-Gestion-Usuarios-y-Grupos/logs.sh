d8c067ebd24b4cd7218ccdd263a58aea33268448
DarkKnight138 <franciscorodriguezalayon2018@gmail.com>
2025-11-10 08:17:14 -0300

    Update logs.sh

--- Servidor/Script-Gestion-Usuarios-y-Grupos/logs.sh
+++ Servidor/Script-Gestion-Usuarios-y-Grupos/logs.sh
@@ -1,1 +1,20 @@
-
+#!/bin/bash
+clear
+opcion=1
+while ((opcion != 0))
+do
+    echo "***************************************************************"
+    echo "*                    Seleccione una opción                    *"
+    echo "* 1) Mostrar logs de Usuarios/Grupos                          *"
+    echo "* 2) Mostrar logs de Respaldos                                *"
+    echo "* 3) Mostrar logs del Firewall                                *"
+    echo "* 0) Salir                                                    *"
+    echo "***************************************************************"
+    read opcion
+    clear
+    case $opcion in
+        1) cat /var/log/user&grupos.log ;;
+        2) cat /var/log/respaldos.log;;
+        3) cat /var/log/firewall.log ;;
+    esac
+done
