5f73568790c0f878818d4b8843ee4c9b747d1312
DarkKnight138 <franciscorodriguezalayon2018@gmail.com>
2025-11-10 09:13:55 -0300

    Update respaldoremoto.sh

--- Servidor/Script-Gestion-Usuarios-y-Grupos/respaldoremoto.sh
+++ Servidor/Script-Gestion-Usuarios-y-Grupos/respaldoremoto.sh
@@ -8,4 +8,6 @@ read ip
 echo "Ingrese credenciales del usuario:"
 read usuario
 scp -r $usuario@$$ip $dir /var/backups
-tar -czf /var/backups/$(date +"%d/%m/%y %H:%M:%S")
+tar -czf /var/backups/$(date +"%d/%m/%y %H:%M:%S").tar.gz /var/backups/$dir
+rm -r /var/backups/$dir
+echo "$(date +"%d/%m/%y %H:%M:%S") Se realizo un respaldo remoto de la ip $ip" >> /var/log/respaldos.log
