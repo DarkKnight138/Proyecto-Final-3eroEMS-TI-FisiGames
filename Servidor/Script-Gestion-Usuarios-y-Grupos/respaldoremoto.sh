d90bbd6b8bfbb0d8b6e7050a94806b7476e5405e
DarkKnight138 <franciscorodriguezalayon2018@gmail.com>
2025-11-10 09:04:09 -0300

    Update respaldoremoto.sh

--- Servidor/Script-Gestion-Usuarios-y-Grupos/respaldoremoto.sh
+++ Servidor/Script-Gestion-Usuarios-y-Grupos/respaldoremoto.sh
@@ -1,1 +1,10 @@
 
+#!/bin/bash
+clear
+echo "Ingrese ruta absoluta del directorio a respaldar:"
+read dir
+echo "Ingrese ip del equipo:"
+read ip
+echo "Ingrese credenciales del usuario:"
+read usuario
+scp $usuario@$$ip $dir 
