58c3ed90036f3ce44b4a01ebaf8e0b7a6b4eb780
DarkKnight138 <franciscorodriguezalayon2018@gmail.com>
2025-11-10 22:44:36 -0300

    Update docker.sh

--- Servidor/Script-Gestion-Usuarios-y-Grupos/docker.sh
+++ Servidor/Script-Gestion-Usuarios-y-Grupos/docker.sh
@@ -1,1 +1,21 @@
-
+#!/bin/bash
+clear
+opcion=1
+while ((opcion != 0))
+do
+    echo "***************************************************************"
+    echo "*                    Seleccione una opción                    *"
+    echo "* 1) Iniciar aplicación                                       *"
+    echo "* 2) Detener aplicación                                       *"
+    echo "* 3) Iniciar Apache                                           *"
+    echo "* 4) Detener Apache                                           *"
+    echo "* 5) Iniciar MySQL                                            *"
+    echo "* 6) Detener MySQL                                            *"
+    echo "* 0) Salir                                                    *"
+    echo "***************************************************************"
+    read opcion
+    clear
+    case $opcion in
+        *)echo "La opción $opcion no es correcta." ;;
+    esac
+done
